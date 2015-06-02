a3327b089d13dcebb942c94c67e9203201a7e6ca
akonyer <akonyer@live.com>
2015-06-01 21:14:04 -0600

    Change random number generator - use System.Security.Cryptography - more secure

--- DicewareNet/Dice/CLRRandom.cs
+++ DicewareNet/Dice/CLRRandom.cs
@@ -1,30 +1,44 @@
-﻿using System;
+﻿#region Includes
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
+#endregion
+
 namespace DicewareNet.Dice
 {
-    public class ClrRandom : IRandom
-    {
-        private readonly Random _rng;
+   public class ClrRandom : IRandom
+   {
+      #region Fields
 
-        public ClrRandom()
-        {
-            _rng = new Random();
-        }
+      private readonly CryptoRandom _rng;
 
-        public long DiceRoll(int numberOfDice)
-        {
-            var diceRolls = new List<int>(numberOfDice);
-            diceRolls.AddRange(Enumerable.Range(0, numberOfDice).Select(_ => _rng.Next(1, 7)));
+      #endregion
+      #region Constructor(s), Destructor, and Dispose
 
-            long finalNumber = 0;
-            for (var power = 0; power < numberOfDice; power++)
-            {
-                finalNumber += (long)(diceRolls[(numberOfDice - 1) - power] * Math.Pow(10.0, (double)power));
-            }
+      public ClrRandom()
+      {
+         _rng = new CryptoRandom();
+      }
 
-            return finalNumber;
-        }
-    }
+      #endregion
+      #region IRandom Members
+
+      public long DiceRoll(int numberOfDice)
+      {
+         var diceRolls = new List<int>(numberOfDice);
+         diceRolls.AddRange(Enumerable.Range(0, numberOfDice).Select(_ => _rng.Next(1, 7)));
+
+         long finalNumber = 0;
+         for(var power = 0; power < numberOfDice; power++)
+         {
+            finalNumber += (long)(diceRolls[(numberOfDice - 1) - power] * Math.Pow(10.0, power));
+         }
+
+         return finalNumber;
+      }
+
+      #endregion
+   }
 }
